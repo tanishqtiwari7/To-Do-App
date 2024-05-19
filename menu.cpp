d5e8cf05fb45cd65534ef18b6279953081d1c0c0
KAKASHI <tanishqtiwari2020@gmail.com>
2024-05-19 11:44:40 +0530

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+#include <iostream>
+
+using namespace std;
+
+class Menu
+{
+public:
+   string x;
+   int menu()
+   {
+
+      std::cout << "\n\n\t\t\t***********************************************\n";
+      cout << "\t\t\t\t\t Saves: a.\n";
+      cout << "\t\t\t\t\t Checked: b.\n";
+      cout << "\t\t\t\t\t New ToDo: c.\n";
+      std::cout << "\t\t\t***********************************************\n";
+   }
+=======
+#include <iostream>
+
+using namespace std;
+
+class Menu
+{
+public:
+   string x;
+   int menu()
+   {
+
+      std::cout << "\n\n\t\t\t***********************************************\n";
+      cout << "\t\t\t\t\t Saves: a.\n";
+      cout << "\t\t\t\t\t Checked: b.\n";
+      cout << "\t\t\t\t\t New ToDo: c.\n";
+      std::cout << "\t\t\t***********************************************\n";
+   }
+>>>>>>> 48719e305426b5ba73b85a15ba736465372c200a
+};
---
+++ new_todo.cpp
@@ -0,0 +1,94 @@
+<<<<<<< HEAD
+#include <iostream>
+
+// #include "menu.cpp"
+#include <string>
+#include <fstream>
+using namespace std;
+
+class New
+{
+public:
+    string task[10];
+    int r = 1;
+    int i = 0;
+    int o = 0;
+    char t;
+
+    int write()
+    {
+
+        ofstream info_in;
+        info_in.open("datacenter.txt");
+        cout << "Write:-\n\n";
+        do
+        {
+
+            cout << r << ". ";
+
+            // std::cin >> task[i];
+            cin.ignore();
+
+            getline(cin, task[i]);
+
+            info_in << task[i] << endl;
+
+            cout << "\nWant to write another ? ";
+            cout << "\nY or N:\n";
+            std::cin >> t;
+
+            r++;
+            i++;
+            o++;
+
+        } while ((t == 'y') || (t == 'Y'));
+        info_in.close();
+    }
+=======
+#include <iostream>
+
+// #include "menu.cpp"
+#include <string>
+#include <fstream>
+using namespace std;
+
+class New
+{
+public:
+    string task[10];
+    int r = 1;
+    int i = 0;
+    int o = 0;
+    char t;
+
+    int write()
+    {
+
+        ofstream info_in;
+        info_in.open("datacenter.txt");
+        cout << "Write:-\n\n";
+        do
+        {
+
+            cout << r << ". ";
+
+            // std::cin >> task[i];
+            cin.ignore();
+
+            getline(cin, task[i]);
+
+            info_in << task[i] << endl;
+
+            cout << "\nWant to write another ? ";
+            cout << "\nY or N:\n";
+            std::cin >> t;
+
+            r++;
+            i++;
+            o++;
+
+        } while ((t == 'y') || (t == 'Y'));
+        info_in.close();
+    }
+>>>>>>> 48719e305426b5ba73b85a15ba736465372c200a
+};
